d403510a37aa8b5b9429fcce2c0277932bf9cb95
Raznor <62889081+Raznor@users.noreply.github.com>
2020-04-10 18:34:59 +0200

    solved exercise two

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
-$('button').click(() => {
-    console.log("Yeah, you clicked me.")
+$('#button-1').click(() => {
+    console.log("Yeah, you clicked me.");
+});
+
+$('#button-2').click(() => {
+    $('#button-1').text("Changed button text! Click me!");
 });
